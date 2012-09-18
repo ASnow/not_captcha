a67866a2cc7cdbdc86f220a3694f818e423fb565
asnow <asnow.dev@gmail.com>
2012-09-18 10:58:49 +0400

    path change

--- lib/not_captcha/image.rb
+++ lib/not_captcha/image.rb
@@ -1,22 +1,22 @@
 module NotCaptcha
-  module Image
+  class Image
     def self.get_random_image_path
-      images[Random.rand(0...images.size)]
+      NotCaptcha::Image.images[Random.rand(0...NotCaptcha::Image.images.size)]
     end
 
     def self.get_composite_name image, answer
       original_name = File.basename(image, File.extname(image))
-      original_image = Image.new(image)
-      width = original_image.columns
+      original_image = MiniMagick::Image.open(image)
+      width = original_image[:width] 
 
       composite_name = "#{original_name}_#{answer}"
       unless File.exists? composite_path(composite_name)
-        composite_image = Image.new(composite_path(composite_name))
+        composite_image = MiniMagick::Image.open(composite_path(composite_name))
         
         0.upto(7) do |i|
           deg = i < answer ? 360-(answer-i)*45 : (i-answer)*45
           rotated_image = original_image.rotate(deg)
-          composite_image.composite(i*width, 0, 0, 0, rotated_image)
+          composite_image = composite_image.composite(i*width, 0, 0, 0, rotated_image)
         end
         composite_image.write composite_path composite_name
       end
@@ -29,7 +29,7 @@ module NotCaptcha
     end
 
     def self.composite_path image_name
-      "/tmp/rotated/#{image_name}.jpg"
+      "#{Rails.root}/lib/not_captcha/rotated/#{image_name}.jpg"
     end
 
     protected
